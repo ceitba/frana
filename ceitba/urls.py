129196cf209f7dfea04e4988d853909ba219dbc4
Federico Bond <federicobond@gmail.com>
2016-01-19 00:34:29 -0300

    Add better error pages

--- ceitba/urls.py
+++ ceitba/urls.py
@@ -1,9 +1,10 @@
 # coding: utf-8
 """ceitba URL Configuration"""
 
+from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
-from django.views.generic.base import RedirectView
+from django.views.generic.base import TemplateView, RedirectView
 import django.contrib.auth.views as auth_views
 
 urlpatterns = [
@@ -15,5 +16,13 @@ urlpatterns = [
         {'template_name': 'bookings/password_change_done.html'}, name='password_change_done'),
     url(r'^admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += [
+        url(r'^404', TemplateView.as_view(template_name='404.html')),
+        url(r'^500', TemplateView.as_view(template_name='500.html')),
+        url(r'^400', TemplateView.as_view(template_name='400.html')),
+        url(r'^403', TemplateView.as_view(template_name='403.html')),
+    ]
 
 admin.site.site_header = 'Administración de Reservas Frana'
